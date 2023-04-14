704b1ef15e20fcf6d78f1799aef4afd83d8aac99
wahsandaruwan <wahsandaruwan6@gmail.com>
2023-04-15 02:04:01 +0530

    Completed the design

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,23 @@
 import { useState } from "react";
+import Todo from "./components/Todo";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div
-        style={{
-          width: "500px",
-          padding: "20px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <input
-          type="text"
-          style={{ width: "70%", padding: "10px", outline: "none" }}
-        />
-        <button
-          style={{
-            width: "25%",
-            padding: "10px",
-            textAlign: "center",
-            marginLeft: "15px",
-            backgroundColor: "#fcba03",
-            border: "1px solid #fcba03",
-            cursor: "pointer",
-          }}
-        >
-          Save
-        </button>
+    <div className="app">
+      <h2 className="title">
+        Simple <span>Todo</span> App
+      </h2>
+      <div className="todo-input-area">
+        <input className="todo-input-box" type="text" />
+        <button className="todo-save-btn">Save</button>
       </div>
-      <div
-        style={{
-          maxWidth: "550px",
-          maxHeight: "700px",
-          padding: "20px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexDirection: "column",
-          backgroundColor: "red",
-          overflowY: "auto",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "10px 0 10px 0",
-            backgroundColor: "#fcba03",
-            padding: "10px",
-          }}
-        >
-          <p>This is my sample tododdddddddddddddddddddddddddddddddddddd.</p>
-          <button
-            style={{
-              padding: "10px",
-              marginRight: "10px",
-            }}
-          >
-            Edit
-          </button>
-          <button
-            style={{
-              padding: "10px",
-            }}
-          >
-            Delete
-          </button>
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            margin: "10px 0 10px 0",
-            backgroundColor: "#fcba03",
-            padding: "10px",
-          }}
-        >
-          <p>This is my sample tododdddddddddddddddddddddddddddddddddddd.</p>
-          <button
-            style={{
-              padding: "10px",
-              marginRight: "10px",
-            }}
-          >
-            Edit
-          </button>
-          <button
-            style={{
-              padding: "10px",
-            }}
-          >
-            Delete
-          </button>
-        </div>
+      <div className="todo-display-area">
+        <Todo />
+        <Todo />
+        <Todo />
+        <Todo />
       </div>
     </div>
   );
---
+++ src/components/Todo.jsx
@@ -0,0 +1,12 @@
+const Todo = () => {
+  return (
+    <div className="todo-item">
+      <div className="todo-body">
+        This is my sample tododdddddddddddddddddddddddddddddddddddd.
+      </div>
+      <button className="todo-edit-btn">Edit</button>
+      <button className="todo-delete-btn">Delete</button>
+    </div>
+  );
+};
+export default Todo;

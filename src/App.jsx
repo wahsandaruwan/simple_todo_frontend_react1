14aece56dc7129b9f26a5ab5786d65eee9136524
wahsandaruwan <wahsandaruwan6@gmail.com>
2023-04-16 21:49:47 +0530

    Added add, update, delete, view features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+// ----------Inbuilt components and modules----------
 import { useState, useEffect } from "react";
-import Todo from "./components/Todo";
+
+// ----------Third-party components and modules----------
 import axios from "axios";
 import uuid from "react-uuid";
+
+// ----------Custom components and modules----------
+import Todo from "./components/Todo";
 
 function App() {
   // All todos state
   const [todos, setTodos] = useState([]);
+
+  // Single todo state
+  const [todo, setTodo] = useState({
+    todoId: 0,
+    todoTitle: "",
+  });
+
+  // Add or update status
+  const [status, setStatus] = useState(0);
 
   // Run once
   useEffect(() => {
@@ -13,12 +27,48 @@ function App() {
     loadTodos();
   }, []);
 
+  // Function for clearing states
+  const clearAll = () => {
+    setTodo({ todoId: 0, todoTitle: "" });
+    setStatus(0);
+  };
+
   // Function for loading all todos
   const loadTodos = async () => {
     try {
-      let res = await axios.get("https://localhost:7038/api/todo/getall");
+      let res = await axios.get(`https://localhost:7038/api/todo/getall`);
       setTodos(res.data);
     } catch (exception) {
+      console.log(exception.response);
+    }
+  };
+
+  // Function for saving a todo
+  const saveTodo = async () => {
+    if (todo.todoTitle == "") {
+      alert("Enter todo title!");
+      return;
+    }
+    try {
+      // To update
+      if (status == 1) {
+        await axios.put(
+          `https://localhost:7038/api/todo/update/${todo.todoId}`,
+          todo
+        );
+        alert("Sucessfully updated!");
+      }
+      // To add
+      else {
+        await axios.post(`https://localhost:7038/api/todo/create`, todo);
+        alert("Sucessfully added!");
+      }
+      // Clear
+      clearAll();
+      // Load
+      loadTodos();
+    } catch (exception) {
+      alert("Operation failed!");
       console.log(exception.response);
     }
   };
@@ -29,12 +79,29 @@ function App() {
         Simple <span>Todo</span> App
       </h2>
       <div className="todo-input-area">
-        <input className="todo-input-box" type="text" />
-        <button className="todo-save-btn">Save</button>
+        <input
+          className="todo-input-box"
+          type="text"
+          value={todo.todoTitle}
+          onChange={(e) => setTodo({ ...todo, todoTitle: e.target.value })}
+        />
+        <button className="todo-save-btn" onClick={() => saveTodo()}>
+          Save
+        </button>
+        <button className="todo-reset-btn" onClick={() => clearAll()}>
+          Reset
+        </button>
       </div>
       <div className="todo-display-area">
-        {todos?.map((item, index) => (
-          <Todo title={item.todoTitle} key={uuid()} />
+        {todos?.map((item) => (
+          <Todo
+            id={item.todoId}
+            title={item.todoTitle}
+            todo={(data) => setTodo(data)}
+            status={(data) => setStatus(data)}
+            load={() => loadTodos()}
+            key={uuid()}
+          />
         ))}
       </div>
     </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,10 +1,43 @@
-const Todo = ({ title }) => {
+// ----------Inbuilt components and modules----------
+import { useState } from "react";
+
+// ----------Third-party components and modules----------
+import axios from "axios";
+
+const Todo = ({ id, title, todo, status, load }) => {
+  // Function for fetching a single todo
+  const fetchTodo = async () => {
+    try {
+      let res = await axios.get(
+        `https://localhost:7038/api/todo/getbyid/${id}`
+      );
+      todo(res.data);
+      status(1);
+    } catch (exception) {
+      console.log(exception.response);
+    }
+  };
+
+  // Function for deleting a todo
+  const deleteTodo = async () => {
+    try {
+      await axios.delete(`https://localhost:7038/api/todo/delete/${id}`);
+      load();
+    } catch (exception) {
+      console.log(exception.response);
+    }
+  };
+
   return (
     <div className="todo-item">
       <div className="todo-body">{title}</div>
       <div>
-        <button className="todo-edit-btn">Edit</button>
-        <button className="todo-delete-btn">Delete</button>
+        <button className="todo-edit-btn" onClick={() => fetchTodo()}>
+          Edit
+        </button>
+        <button className="todo-delete-btn" onClick={() => deleteTodo()}>
+          Delete
+        </button>
       </div>
     </div>
   );
